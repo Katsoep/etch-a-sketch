8c93225a4f82ade0b80a642f41d846344cd70fe4
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-01-19 19:31:20 +0100

    build (core) set up reset btn

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,28 @@
 const gridCon = document.querySelector(".square-con");
 const gridInfo = gridCon.getBoundingClientRect();
+const resetBtn = document.querySelector("#reset-btn");
+const log = document.querySelector("#log");
+log.innerText = `hit "reset to start a new grid`;
+
+resetBtn.addEventListener("click", () => {
+    let size = prompt("How big would you like your grid? (max 100)");
+
+     if (size !== null) {
+        console.log(size);
+        parseInt(size);
+         if (size <= 100){
+            console.log("im a correct number");
+            log.innerText = (size + " x " + size);
+            createGrid();
+        } else {
+            console.log(size);
+            log.innerText = "Not a valid amount";
+        }
+     } else if (size === null) {
+        log.innerText = "Nothing was entered.";
+     }
+
+})
 
 for (let i = 0; i < 16; i++) {
     const newDiv = document.createElement("div");
@@ -10,7 +33,10 @@ for (let i = 0; i < 16; i++) {
 const singleSquares = document.querySelectorAll(".single-square");
 singleSquares.forEach ( square => {
     square.addEventListener("mouseenter", () => {
-        console.log("Yes");
         square.classList.add("color-me");
     })
 });
+
+function createGrid () {
+
+}
