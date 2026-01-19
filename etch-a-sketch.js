530a8b787080cc9618ef092a3fdd788c6c0b7c53
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-01-19 14:21:35 +0100

    build (core) add hover changes bg-color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,16 @@
 const gridCon = document.querySelector(".square-con");
 const gridInfo = gridCon.getBoundingClientRect();
-//make new div x amount of times
+
 for (let i = 0; i < 16; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("single-square");
-    // newDiv.style.minWidth = "230px";
-    // newDiv.style.minHeight = "230px";
     gridCon.appendChild(newDiv);
 }
 
+const singleSquares = document.querySelectorAll(".single-square");
+singleSquares.forEach ( square => {
+    square.addEventListener("mouseenter", () => {
+        console.log("Yes");
+        square.classList.add("color-me");
+    })
+});
