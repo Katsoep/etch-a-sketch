7c628d168d7c8f402d0906769d75d3cecc687597
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-01-20 23:22:35 +0100

    build (fix) issue with branch merge conflict, solved an error in terminal

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,50 @@
 const gridCon = document.querySelector(".square-con");
 const gridInfo = gridCon.getBoundingClientRect();
+const resetBtn = document.querySelector("#reset-btn");
+const log = document.querySelector("#log");
+log.innerText = `hit "reset to start a new grid`;
 
-for (let i = 0; i < 16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("single-square");
-    gridCon.appendChild(newDiv);
-}
+resetBtn.addEventListener("click", () => {
+    let size = prompt("How big would you like your grid? (max 100)");
 
-const singleSquares = document.querySelectorAll(".single-square");
-singleSquares.forEach ( square => {
-    square.addEventListener("mouseenter", () => {
-        console.log("Yes");
-        square.classList.add("color-me");
-    })
+     if (size !== null) {
+        parseInt(size);
+         if (size <= 100){
+            log.innerText = (size + " x " + size);
+            // let quotient = getQuotient(size);
+            // console.log(quotient);
+            createGrid(+size);
+        } else {
+            log.innerText = "Not a valid amount";
+        }
+     } else if (size === null) {
+        log.innerText = "Nothing was entered.";
+     }
+
+})
+
+function createGrid (amount) {
+      console.log("im the amount " + amount);
+    while (gridCon.hasChildNodes()) {
+        gridCon.removeChild(gridCon.firstChild);
+    }
+
+    for (let i = 0; i < amount; i++) {
+    const subCon = document.createElement("div");
+    subCon.classList.add("row-square");
+    gridCon.appendChild(subCon);
+        for (let i = 0; i < amount; i++){
+            const square = document.createElement("div");
+            square.classList.add("single-square");
+            subCon.appendChild(square);
+        }
+    }
+     //change color on hover
+    const singleSquares = document.querySelectorAll(".single-square");
+    singleSquares.forEach ( square => {
+        square.addEventListener("mouseenter", () => {
+            square.classList.add("color-me");
+        })
 });
 }
 
@@ -24,8 +56,4 @@ function getQuotient (promptNum) {
     return quotient;
     }  
     }
-}
-
-function createGrid () {
-
 }
