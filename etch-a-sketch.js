90f30a8918d3758ecd33222a9bec21ac2849ee28
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-01-21 15:08:37 +0100

    build (core) random color + trail fade in mouse hover fades in opacity and chooses a random background color for the square. after short delay the square reset. opacity only resets after all squares are reset to blank.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,7 @@ const gridInfo = gridCon.getBoundingClientRect();
 const resetBtn = document.querySelector("#reset-btn");
 const log = document.querySelector("#log");
 log.innerText = `hit "reset to start a new grid`;
+let overallOpacity = 0;
 
 resetBtn.addEventListener("click", () => {
     let size = prompt("How big would you like your grid? (max 100)");
@@ -43,17 +44,38 @@ function createGrid (amount) {
     const singleSquares = document.querySelectorAll(".single-square");
     singleSquares.forEach ( square => {
         square.addEventListener("mouseenter", () => {
-            square.classList.add("color-me");
+
+            //reset opacity to 0 if no squares are colored
+            let active = document.querySelectorAll("#active");
+            if(active.length === 0 && overallOpacity > 0){
+                    overallOpacity = 0;
+                }
+            square.style.backgroundColor = randomRgbColor();
+            square.setAttribute("id", "active");
+
+            //after delay reset square
+            setTimeout(() => {
+                square.setAttribute("id", "inactive");
+                square.style.backgroundColor = "rgba(355,355,355,1)";
+            }, 1000);
         })
-});
+    });
 }
 
-function getQuotient (promptNum) {
-    for (let i = 1; i < promptNum; i++){
-    if(promptNum % i === 0){
-    let quotient = promptNum;
-     console.log(quotient);
-    return quotient;
-    }  
+const randomRgbColor = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256); 
+    let b = Math.floor(Math.random() * 256); 
+    let a = round((overallOpacity += 0.1), 1).toFixed(1); 
+    console.log(a);
+    return "rgba(" + r + "," + g + "," + b +  "," + a + ")";
+};
+
+function round(value, precision) {
+    if (value < 1){
+        let multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+    } else{
+        return 1;
     }
 }
